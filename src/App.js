385173a32f5721fefb100e69fa2e83be866dd0fd
Iskender-Garajayev <yoloten.isa@gmail.com>
2023-03-29 09:06:17 +0300

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import Header from './components/Header';
-import ModulS from './components/ModulS';
-import ModelY from './components/ModelY';
-import Model3 from './components/Model3';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Model3/>
-      <ModulS/>
-      <ModelY/>
+      <Homepage/>
     </div>
   );
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Model3 from './Model3'
+import ModelY from './ModelY'
+import ModulS from './ModulS'
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header/>
+      <Model3/>
+      <ModulS/>
+      <ModelY/>
+    </div>
+  )
+}
+
+export default Homepage
